410a16f5844225f3eaf8d95f22156e0b7a1172d4
zainafzal-tek <zainafzal@gmail.com>
2020-08-18 22:20:11 -0400

    day43_Static Cont.

---
+++ src/day43_Static/Test.java
@@ -0,0 +1,20 @@
+package day43_Static;
+
+import java.util.Arrays;
+import static Library.Resources.a;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(StaticBlock3.a);
+        System.out.println(StaticBlock3.b);
+        System.out.println(StaticBlock3.c);
+
+        System.out.println( Arrays.toString(Data.arr) );
+        System.out.println(Data.list);
+
+
+    }
+
+
+}
