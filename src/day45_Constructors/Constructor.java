ec479c99da2bbced7d671711593d0bade905909d
zainafzal-tek <zainafzal@gmail.com>
2020-08-26 19:09:30 -0400

    day45 Constructors Cont.

---
+++ src/day45_Constructors/Constructor.java
@@ -0,0 +1,34 @@
+package day45_Constructors;
+
+public class Constructor {
+
+    public Constructor(){
+        System.out.println("No argument");
+    }
+
+    public Constructor(int a){
+        System.out.println("int argument");
+    }
+
+    public Constructor(String a){
+        System.out.println("String argument");
+    }
+
+    public Constructor(int a, int b){
+        System.out.println("two int arguments");
+    }
+
+    public static void main(String[] args) {
+
+        Constructor obj1 = new Constructor(10, 20);
+
+
+     /*
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.addAll(Arrays.asList(1,2,3,4,5));
+        ArrayList<Integer> list2 = new ArrayList<>(list1);
+        System.out.println(list2);
+        */
+
+    }
+}
