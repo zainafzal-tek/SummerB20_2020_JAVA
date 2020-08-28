abef76d8dad5295b82ed8d80865e206530b9cad6
zainafzal-tek <zainafzal@gmail.com>
2020-08-27 20:36:29 -0400

    Update test

--- src/Day0/Test.java
+++ src/Day0/Test.java
@@ -1,14 +1,5 @@
 package Day0;
 
-import java.lang.reflect.Array;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Test {
 
     public static void main(String[] args) {
