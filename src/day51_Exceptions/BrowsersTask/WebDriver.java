7e3916d72fbc53943c33289c49765d8573844b84
zainafzal-tek <zainafzal@gmail.com>
2020-09-06 10:09:38 -0400

    Day 51 Exceptions Class

---
+++ src/day51_Exceptions/BrowsersTask/WebDriver.java
@@ -0,0 +1,22 @@
+package day51_Exceptions.BrowsersTask;
+
+public class WebDriver {
+
+    /*
+1. create a class called WebDriver
+            actions: get(URL): prints "opening the URL in the default browser"
+                     close(): closes the default browser
+ */
+
+    public class Webdriver {
+
+        protected void get(String url) {
+            System.out.println("Opening the " + url + " in default browser");
+        }
+
+        public void close() {
+            System.out.println("closing the default browser");
+        }
+
+    }
+}
