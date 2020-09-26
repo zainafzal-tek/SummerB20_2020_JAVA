3697496ff76a4bc7e698a08c183156dee295d6b9
zainafzal-tek <zainafzal@gmail.com>
2020-09-26 14:15:18 -0400

    Day 58 Polymorphism

--- src/day58_Polymorphism/InstanceOf_Keyword.java
+++ src/day58_Polymorphism/InstanceOf_Keyword.java
@@ -1,5 +1,9 @@
 package day58_Polymorphism;
 
+import day55_Abstraction.ShapeTask.Circle;
+import day55_Abstraction.ShapeTask.Cylinder;
+import day55_Abstraction.ShapeTask.Rectangle;
+import day55_Abstraction.ShapeTask.Shape;
 import day58_Polymorphism.AnimalTask.Animal;
 import day58_Polymorphism.AnimalTask.Cat;
 import day58_Polymorphism.AnimalTask.Dog;
@@ -16,6 +20,35 @@ public class InstanceOf_Keyword {
             System.out.println("True");
         } else {
             System.out.println("False");
+        }
+
+        // verify animal3 is the object of Dog class
+        boolean r3 = animal3 instanceof Dog; // there is no "IS A relation" ==> false
+        System.out.println(r3);
+
+        boolean r4 = animal2 instanceof Animal;  // Dog is Animal
+        System.out.println(r4);
+
+        System.out.println(animal2 instanceof Dog); // Dog is Dog
+
+        System.out.println(animal1 instanceof Dog); // Animal is A Dog
+
+        System.out.println(animal2 instanceof Cat); // Dog is Cat
+
+        System.out.println("==============================================");
+
+        Shape shape1 = new Rectangle(3, 5);
+        identifyShape(shape1);
+
+    }
+
+    public static void identifyShape(Shape shape){
+        if(shape instanceof Circle){
+            System.out.println("The shape is circle");
+        }else if(shape instanceof Cylinder){
+            System.out.println("The shape is cylinder");
+        }else{
+            System.out.println("The shape is rectangle");
         }
 
     }
