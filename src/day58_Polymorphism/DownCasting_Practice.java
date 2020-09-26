5cac0c508e54ebff4ae6d049ab6ff6d7ab22800a
zainafzal-tek <zainafzal@gmail.com>
2020-09-26 16:26:14 -0400

    Day 58 Polymorphism

--- src/day58_Polymorphism/DownCasting_Practice.java
+++ src/day58_Polymorphism/DownCasting_Practice.java
@@ -1,6 +1,7 @@
 package day58_Polymorphism;
 
 import day57_Polymorphism.PhoneTasks.Phone;
+import day57_Polymorphism.PhoneTasks.Samsung;
 import day57_Polymorphism.PhoneTasks.iPhone;
 
 public class DownCasting_Practice {
@@ -9,8 +10,17 @@ public class DownCasting_Practice {
 
         Phone phone1 = new iPhone("11", "6", 1000);
         iPhone iphone1 = (iPhone) phone1;
+
+        //phone 1 facetiming after downcasting to iphone
         iphone1.faceTiming(1234);
 
+        //second way
+        ((iPhone)phone1).faceTiming(1234556);
+
+        System.out.println("========================================");
+        Phone phone2 = new Samsung("S20", "7", 1000);
+
+        ((Samsung)phone2).googleDuo(12345678);
 
     }
 
