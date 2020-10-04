15cbea53f316d45be02f4193d9a9237d17eaf9ae
zainafzal-tek <zainafzal@gmail.com>
2020-10-04 10:09:55 -0400

    Day 60 Collections

--- src/day60_Collections/NullKeyword.java
+++ src/day60_Collections/NullKeyword.java
@@ -1,2 +1,18 @@
-package day60_Collections;public class NullKeyword {
+package day60_Collections;
+
+public class NullKeyword {
+
+    public static void main(String[] args) {
+
+        String s1 = "Cybertek";
+
+        System.out.println( s1.toUpperCase() );
+
+        String s2 = null;
+
+        System.out.println( s2.toUpperCase() );
+
+    }
+
+
 }
--- src/day60_Collections/QueueInterface.java
+++ src/day60_Collections/QueueInterface.java
@@ -1,2 +1,29 @@
-package day60_Collections;public class QueueInterface {
+package day60_Collections;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+public class QueueInterface {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> q1 = new ArrayDeque<>();
+        q1.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        q1.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+        System.out.println(q1);
+
+        int firstObject = q1.poll();
+
+        System.out.println(q1);
+
+        q1.poll();
+
+        System.out.println(q1);
+
+        //  q1.get(2); // queue has no index numbers
+
+    }
+
 }
