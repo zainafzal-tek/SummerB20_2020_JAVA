2ef7ecc221977c5785f37c9738b6b8c4f3a2974c
Zain_Afzal <zainafzal@gmail.com>
2020-10-07 19:10:40 -0400

    add day 61

---
+++ src/day61_Maps/RemoveMaps.java
@@ -0,0 +1,20 @@
+package day61_Maps;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RemoveMaps {
+
+    public static void main(String[] args) {
+
+        String[] words = {"Java", "Python", "Level", "Kayak", "Cybertek", "Zoman", "Ana", "Batch 20"};
+
+        List<String> list = new ArrayList<>();
+        list.addAll(Arrays.asList(words));
+
+        System.out.println(list);
+
+    }
+}
